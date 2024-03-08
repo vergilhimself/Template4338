65745978529d1c4a56dc6a1648009b5ca07bdd5b
Untitled <fidanclasher2005@gmail.com>
2024-03-08 11:57:19 +0300

    Db added

---
+++ Template4338/Template4338/Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Template4338
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Table> Tables { get; set; }
+    }
+
+    public class Table
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
